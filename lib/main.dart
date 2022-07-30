7b1552fb336eb3981dc3d88256c11f189a9ff31b
zakreya_omar <zakreyao@gmail.com>
2022-07-30 14:10:04 +0300

    change title in main screen commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DemoSquadio',
+      title: 'DemoSqu',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
